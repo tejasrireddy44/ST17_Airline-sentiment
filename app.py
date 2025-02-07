71b0827895d79abfe38957e1243024f2e99a7647
saicharanveeramalla0@gmail.com <saicharanveeramalla0@gmail.com>
2025-02-07 12:55:32 +0530

    DATA

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template,request
+app=Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+@app.route("/predict",methods=["POST","Get"])
+def predict():
+    if request.method=="POST":
+       msg=request.form.get("message")
+       print(msg)
+    else:
+        return render_template("predict.html")
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",port=5151)
