98ac39e478dbdf6dd3bc4e6d03bb2de566051905
saicharanveeramalla0@gmail.com <saicharanveeramalla0@gmail.com>
2025-02-10 10:28:02 +0530

    asd

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template,request
+import pickle
 app=Flask(__name__)
 
 @app.route("/")
@@ -7,8 +8,27 @@ def index():
 @app.route("/predict",methods=["POST","Get"])
 def predict():
     if request.method=="POST":
-       msg=request.form.get("message")
-       print(msg)
+        msg=request.form.get("message")
+        print(msg)
+        ob=TextToNum(msg)
+        ob.cleaner()
+        ob.token()
+        ob.removeStop()
+        st=ob.stemme()
+        stem_vector=" ".join(st)
+
+        with open("vectorizer.pickle","rb") as vc:
+            vectorizer=pickle.load(vc)
+        vcdata=vectorizer.transform([stem_vector]).toarray()
+        print(vcdata)
+        
+        with open("model.pickle","rb") as mc:
+            model=pickle.load(mc)
+
+        pred=model.predict(vcdata)
+        print(pred)
+
+
     else:
         return render_template("predict.html")
 
