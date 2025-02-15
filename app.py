67b005727a85b3788f6efacfa2a73376a0c3d9c3
tejasrireddyvangala@gmail.com <tejasrireddy44>
2025-02-15 11:44:40 +0530

    appppppp

--- app.py
+++ app.py
@@ -13,15 +13,19 @@ from flask import Flask, request, jsonify
 nltk_data_path = "/opt/render/project/nltk_data"
 os.makedirs(nltk_data_path, exist_ok=True)
 nltk.data.path.append(nltk_data_path)
+# Download the necessary NLTK data
+nltk.download("punkt", download_dir=nltk_data_path)
+nltk.download("punkt_tab", download_dir=nltk_data_path)  # Fix missing resource
+nltk.download("stopwords", download_dir=nltk_data_path)
+nltk.download("wordnet", download_dir=nltk_data_path)
+
 
 
 
 # Your existing functions (clean_text, analyze_sentiment_ml, etc.)
 
 # Download required NLTK resources
-nltk.download("punkt")
-nltk.download("stopwords")  # If using stopwords
-nltk.download("wordnet")  # If using WordNet Lemmatizer
+ # If using WordNet Lemmatizer
 
 
 # Initialize Flask app
